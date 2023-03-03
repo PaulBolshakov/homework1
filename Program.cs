db2d7f13340afd8a2ebf56f32451706c75926254
Pavel <pavelbolshakovrus@gmail.com>
2023-03-03 15:57:38 +0300

    home work

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿//Task 1 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*Console.Write("input number one: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number two: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+    Console.WriteLine(number1 + "= max and " + number2 + "= min");
+else
+    Console.WriteLine(number1 = "min");*/
+/*
+Console.Write("input a first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+    Console.WriteLine(number1 + " max and " + number2 + " min");
+else
+    Console.WriteLine(number2 + " max and " + number1 + " min");*/
+//  ПОЧЕМУ ПЕРВЫЙ ВАРИАНТ НЕ РАБОТАЕТ А ВТОРОЙ РАБОТАЕТ? НЕ МОГУ ПОНЯТЬ ГДЕ И ЧТО НЕ ТАК!!!
+
+//Task 2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.Write("input number one");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number two");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number three");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if (max < number2)
+{
+    Console.WriteLine(number2 + " = max");
+    max = number2;
+}
+
+if (max < number3)
+{
+    Console.WriteLine(number3 + " = max");
+    max = number3;
+}
+
+else
+    Console.WriteLine(max + " = max");*/
+// НЕ ДОКОНЦА ПОНЯЛ КАК ПРАВИЛЬНО НАПИСАТЬ ДЛЯ КОРРЕКТНОЙ РАБОТЫ КОДА
+
+//Task 3 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+/*Console.Write("input number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+    Console.WriteLine("четное");
+else
+    Console.WriteLine("не четное");*/
+//ТУТ В ОБЩЕМ ТОЛЬКО НАЙТИ ФОРМУЛУ НАДО БЫЛО
+
+//Task 4 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*Console.Write("input number");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+while (current <= number)
+{
+    Console.Write(current + " ");
+    current = current + 2;
+}*/
+//ЛОГИЧЕСКИ ПОСТАВИЛ ЧТО ОТСЧЕТ НАЧАЛ С 2, А МОЖНО КАК ТО ГРАМОТНЕЙ СДЕЛАТЬ ЭТО? 
+
+
+
+
+
+
+
